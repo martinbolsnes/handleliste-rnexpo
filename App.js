bc5e118d54cc3748b7a0a689e2818d31acad8717
martinbolsnes <martinbolsnes@icloud.com>
2023-03-25 15:18:54 +0100

    add fonts & more screens

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import registerNNPushToken from 'native-notify';
+import { useFonts } from 'expo-font';
 
 import Home from './src/screens/Home';
+import Login from './src/screens/Login';
+import SignUp from './src/screens/SignUp';
+import Account from './src/screens/Account';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  registerNNPushToken(6647, 'dgQybD5qqY8UlBaT3rs85x');
+  const [fontsLoaded] = useFonts({
+    'Righteous-Regular': require('./assets/fonts/Righteous-Regular.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter-Medium.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    <NavigationContainer>
+    <NavigationContainer onLayout={onLayoutRootView}>
       <Stack.Navigator>
-        <Stack.Screen name='Home' options={{ headerShown: false }}>
-          {(props) => <Home {...props} />}
-        </Stack.Screen>
+        <Stack.Screen
+          name='Login'
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='SignUp'
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name='Account'
+          component={Account}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
